a99612376a85d67bcbd73e41d7d15a48d69d08d8
makssen <maks.sen.1903@mail.ru>
2021-12-02 11:32:46 +0300

    ref

--- migrations/Version20211202083150.php
+++ migrations/Version20211202083150.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211202080847 extends AbstractMigration
+final class Version20211202083150 extends AbstractMigration
 {
     public function getDescription(): string
     {
