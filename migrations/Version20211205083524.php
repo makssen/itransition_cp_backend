9111c20613f93b403c32cf9f394506e8a6f2159d
makssen <maks.sen.1903@mail.ru>
2021-12-05 11:39:45 +0300

    upgrade

--- migrations/Version20211205083524.php
+++ migrations/Version20211205083524.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211202085327 extends AbstractMigration
+final class Version20211205083524 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,8 @@ final class Version20211202085327 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE user_id_seq CASCADE');
+        $this->addSql('CREATE SEQUENCE overview_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE tag_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE overview (id INT NOT NULL, title VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, tags TEXT NOT NULL, text TEXT NOT NULL, grade INT NOT NULL, images TEXT NOT NULL, date_time TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN overview.tags IS \'(DC2Type:array)\'');
         $this->addSql('COMMENT ON COLUMN overview.images IS \'(DC2Type:array)\'');
@@ -31,7 +32,8 @@ final class Version20211202085327 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('CREATE SEQUENCE user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('DROP SEQUENCE overview_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE tag_id_seq CASCADE');
         $this->addSql('DROP TABLE overview');
         $this->addSql('DROP TABLE tag');
     }
--- migrations/Version20211205083753.php
+++ migrations/Version20211205083753.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211202085646 extends AbstractMigration
+final class Version20211205083753 extends AbstractMigration
 {
     public function getDescription(): string
     {
